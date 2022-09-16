4d248b658450d86abbb32c7c73d910acec70e0b2
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-16 23:40:54 +0530

    Update usePools.js

--- packages/react-app/src/hooks/usePools.js
+++ packages/react-app/src/hooks/usePools.js
@@ -3,23 +3,30 @@ import { useState, useEffect } from 'react';
 import { useConfig } from '@usedapp/core';
 
 import { ROUTER_ADDRESS } from '../config';
+import { getFactoryInfo, getRouterInfo } from '../utils';
 
 export const loadPools = async (providerUrl) => {
+  const provider = new Web3.providers.HttpProvider(providerUrl);
+  const web3 = new Web3(provider);
 
+  const routerInfo = await getRouterInfo(ROUTER_ADDRESS, web3);
+  const factoryInfo = await getFactoryInfo(routerInfo.factory, web3);
+  console.log(factoryInfo);
+  return factoryInfo.pairsInfo;
 };
 
 export const usePools = () => {
-    const { readOnlyChainId, readOnlyUrls } = useConfig();
-    const [loading, setLoading] = useState(true);
-    const [pools, setPools] = useState({});
+  const { readOnlyChainId, readOnlyUrls } = useConfig();
+  const [loading, setLoading] = useState(true);
+  const [pools, setPools] = useState({});
 
-    useEffect(() => {
-        loadPools(readOnlyUrls[readOnlyChainId])
-            .then((pools) => {
-                setPools(pools);
-                setLoading(false);
-            });
-    }, [readOnlyChainId, readOnlyUrls]);
+  useEffect(() => {
+    loadPools(readOnlyUrls[readOnlyChainId])
+      .then((pools) => {
+          setPools(pools);
+          setLoading(false);
+      });
+  }, [readOnlyChainId, readOnlyUrls]);
 
-    return [loading, pools];
+  return [loading, pools];
 };
