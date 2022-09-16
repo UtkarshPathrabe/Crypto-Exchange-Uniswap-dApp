e097cf75ef8499b982a3de7b32fb2df7c955efc0
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-16 21:13:52 +0530

    Update config.js

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0xd7f14025e3e33c4f715bb5bf6cd766585c6a9806a207c34fd788558768260705"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/GlIoggxj8Ub50WBscDlsJPoOa1ddN-tx",
   },
 };
