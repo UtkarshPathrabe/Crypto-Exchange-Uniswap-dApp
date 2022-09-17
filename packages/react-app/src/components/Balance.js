f1d6dd5794d5c748093029d06543ce048ab4d46e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-17 17:29:51 +0530

    Update Balance.js

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
+import styles from '../styles';
 
 const Balance = () => {
-  return (
-    <div>Balance</div>
-  )
-}
+  const tokenBalance = parseUnits("0.00251");
+
+  return (<div className={styles.balance}>
+    <p className={styles.balanceText}>
+      {(tokenBalance)
+        ? <>
+          <span className={styles.balanceBold}>Balance: </span>
+          { formatUnits(tokenBalance || parseUnits("0")) }
+        </>
+        : null}
+    </p>
+  </div>);
+};
 
 export default Balance;
