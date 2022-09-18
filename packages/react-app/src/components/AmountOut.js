64d782b94e997109c4ac9fd6a13d73fedb43286d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-09-18 22:52:19 +0530

    Update AmountOut.js

--- packages/react-app/src/components/AmountOut.js
+++ packages/react-app/src/components/AmountOut.js
@@ -1,29 +1,56 @@
+import { formatUnits } from 'ethers/lib/utils';
 import React, { useState, useRef, useEffect } from 'react';
 
 import { chevronDown } from '../assets';
 import styles from '../styles';
-import { useOnClickOutside } from '../utils';
+import { useOnClickOutside, useAmountsOut } from '../utils';
 
-const AmountOut = () => {
+const AmountOut = ({ fromToken, toToken, amountIn, pairContract, currencyValue, onSelect, currencies }) => {
   const [showList, setShowList] = useState(false);
+  const [activeCurrency, setActiveCurrency] = useState("Select");
+  const ref = useRef();
+
+  const amountOut = useAmountsOut(pairContract, amountIn, fromToken, toToken) ?? 0;
+
+  useOnClickOutside(ref, () => setShowList(false));
+
+  useEffect(() => {
+    if (Object.keys(currencies).includes(currencyValue)) {
+      setActiveCurrency(currencies[currencyValue]);
+    }
+    else {
+      setActiveCurrency("Select");
+    }
+  }, [currencies, currencyValue]);
+
   return (<div className={styles.amountContainer}>
     <input
       placeholder='0.0'
       type='number'
-      value={''}
+      value={formatUnits(amountOut)}
       disabled
       className={styles.amountInput}
     />
     <div className='relative' onClick={() => setShowList((prevState) => !prevState)}>
       <button className={styles.currencyButton}>
-        {'ETH'}
+        {activeCurrency}
         <img src={chevronDown} alt={'chevron down'} className={`w-4 h-4 object-contain ml-2 ${showList ? 'rotate-180' : 'rotate-0'}`} />
       </button>
-      { showList ? (<ul className={styles.currencyList}>
-        {[
-          {token: 'ETH', tokenName: 'ETH'},
-          {token: 'JSM Gold', tokenName: 'JSM Gold'},
-        ].map(({token, tokenName}, index) => (<li key={index} className={`${styles.currencyListItem} ${true ? 'bg-site-dim2' : ''} cursor-pointer`}>{tokenName}</li>))}
+      { showList ? (<ul ref={ref} className={styles.currencyList}>
+        { Object.entries(currencies).map(([ token, tokenName ], index) =>
+          (<li
+            key={index}
+            className={styles.currencyListItem}
+            onClick={() => {
+              if (typeof onSelect === "function") {
+                onSelect(token);
+              }
+              setActiveCurrency(tokenName);
+              setShowList(false);
+            }}
+          >
+            {tokenName}
+          </li>)) }
       </ul>) : null }
     </div>
   </div>);
